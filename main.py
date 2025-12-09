8b32336d3593e35feb8adb5f8df4857c65ff75cb
Montessa Young <montessakayyoung@gmail.com>
2025-12-09 20:49:36 +0000

    add main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # This is the file that you will run to play your game
 # Import all of the separate functions at the top of this file
 # This file should only be function calls.
+from Braysen_base_for_encounters import chance_encounter, rebel_encounter, trade_with_rebels, typing_challenge, handle_danger
+from Kreed_swampchallenges import print_slow, decide_campsite
+
+chance_encounter()
